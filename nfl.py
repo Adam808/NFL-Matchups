caf8b0450153cf7c61d076b5cecb627428c10789
unknown <adamparker05@gmail.com>
2024-11-28 18:05:39 -0500

    added overall summary scores

--- nfl.py
+++ nfl.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# pull website data
 off_passing = pd.read_html('https://www.nfl.com/stats/team-stats/', index_col=False)[0]
 off_passing = off_passing.iloc[:, [0, 1, 3, 4, 5, 7]]
 off_passing.Team = off_passing.Team.str.split()
@@ -44,7 +45,26 @@ def_rushing['Rush Yds Rank'] = def_rushing['Rush Yds'].rank(method='min', ascend
 def_rushing.sort_values(by=['YPC'], ascending=True, inplace=True)
 def_rushing['YPC Rank'] = def_rushing['YPC'].rank(method='min', ascending=True)
 
+# multiples
+multi_dict = {32: 0.5}
+increment = .5/15.5
+start = 32
+start_val = 0.5
+for i in range(15):
+  start -= 1
+  start_val += increment
+  multi_dict[start] = start_val
 
+increment = 1/15.5
+start = 1 
+start_val = 2
+multi_dict[1] = 2
+for i in range(15):
+  start +=1
+  start_val -= increment
+  multi_dict[start] = start_val
+
+# get values
 def matchup(team1, team2):
     print('')
     print('### ' + team1 + ' Rushing vs ' + team2 + ' Defense' + ' ###')
@@ -53,10 +73,12 @@ def matchup(team1, team2):
     ypc = int(off_rushing[off_rushing['Team'] == team1]['YPC Rank'].iloc[0]) - int(def_rushing[def_rushing['Team'] == team2]['YPC Rank'].iloc[0])
     fumble = int(off_rushing[off_rushing['Team'] == team1]['Rush FUM'].iloc[0])
     def_fumble = int(def_rushing[def_rushing['Team'] == team2]['Rush FUM'].iloc[0])
+    summary = (rush_yds + ypc) * multi_dict[att]
     print('Rush Attempts Rank: ' + str(att))
     print('Rush Yds Diff: ' + str(rush_yds))
     print('YPC Diff: ' + str(ypc))
     print('Fumbles: ' + str(fumble) + ', Forced Fumbles: ' + str(def_fumble))
+    print('Overall Score: ' + str(summary))
     print('')
     print('### ' + team1 + ' Passing vs ' + team2 + ' Defense' ' ###')
     pass_att = int(off_passing[off_passing['Team'] == team1]['Att Rank'].iloc[0])
@@ -65,11 +87,13 @@ def matchup(team1, team2):
     comp_per = int(off_passing[off_passing['Team'] == team1]['Cmp % Rank'].iloc[0]) - int(def_passing[def_passing['Team'] == team2]['Cmp % Rank'].iloc[0])
     interceptions = int(off_passing[off_passing['Team'] == team1]['INT'].iloc[0])
     def_interceptions = int(def_passing[def_passing['Team'] == team2]['INT'].iloc[0])
+    summary = (pass_yds + yds_per_att) * multi_dict[pass_att]
     print('Pass Attempts Rank: ' + str(pass_att))
     print('Pass Yds Diff: ' + str(pass_yds))
     print('Yds Per Attempt Diff: ' + str(yds_per_att))
     print('Completion % Diff: ' + str(comp_per))
     print('Interceptions: ' + str(interceptions) + ', Forced Interceptions: ' + str(def_interceptions))
+    print('Overall Score: ' + str(summary))
     print('')
 
     print('### ' + team2 + ' Rushing vs ' + team1 + ' Defense' + ' ###')
@@ -78,10 +102,12 @@ def matchup(team1, team2):
     ypc = int(off_rushing[off_rushing['Team'] == team2]['YPC Rank'].iloc[0]) - int(def_rushing[def_rushing['Team'] == team1]['YPC Rank'].iloc[0])
     fumble = int(off_rushing[off_rushing['Team'] == team2]['Rush FUM'].iloc[0])
     def_fumble = int(def_rushing[def_rushing['Team'] == team1]['Rush FUM'].iloc[0])
+    summary = (rush_yds + ypc) * multi_dict[att]
     print('Rush Attempts Rank: ' + str(att))
     print('Rush Yds Diff: ' + str(rush_yds))
     print('YPC Diff: ' + str(ypc))
     print('Fumbles: ' + str(fumble) + ', Forced Fumbles: ' + str(def_fumble))
+    print('Overall Score: ' + str(summary))
     print('')
     print('### ' + team2 + ' Passing vs ' + team1 + ' Defense' ' ###')
     pass_att = int(off_passing[off_passing['Team'] == team2]['Att Rank'].iloc[0])
@@ -90,11 +116,13 @@ def matchup(team1, team2):
     comp_per = int(off_passing[off_passing['Team'] == team2]['Cmp % Rank'].iloc[0]) - int(def_passing[def_passing['Team'] == team1]['Cmp % Rank'].iloc[0])
     interceptions = int(off_passing[off_passing['Team'] == team2]['INT'].iloc[0])
     def_interceptions = int(def_passing[def_passing['Team'] == team1]['INT'].iloc[0])
+    summary = (pass_yds + yds_per_att) * multi_dict[pass_att]
     print('Pass Attempts Rank: ' + str(pass_att))
     print('Pass Yds Diff: ' + str(pass_yds))
     print('Yds Per Attempt Diff: ' + str(yds_per_att))
     print('Completion % Diff: ' + str(comp_per))
     print('Interceptions: ' + str(interceptions) + ', Forced Interceptions: ' + str(def_interceptions))
+    print('Overall Score: ' + str(summary))
 
 first_team = input('Team 1: ')
 second_team = input('Team 2: ')
